bf2c28a4b2cc6fb4bfb99b130d8b1bf73e003b4f
Shubham Rai <shubham56237@gmail.com>
2023-08-13 13:43:09 +0530

    random joke generated

--- random joke generator/app.js
+++ random joke generator/app.js
@@ -1,15 +1,27 @@
-fetch("https://icanhazdadjoke.com/", {
-  headers: {
-    Accept: "application/json", // Request JSON format
-  },
-})
-  .then((response) => response.json()) // Parse the response as JSON
-  .then((data) => {
-    console.log(data); // Log the joke data
-    // Now you can work with the JSON data
-    const title = document.querySelector(".title")
-    title.textContent = data.joke
-  })
-  .catch((error) => {
-    console.error("Error fetching data:", error);
-  });
+const btn = document.querySelector(".btn")
+
+
+let jokeGenerator = () => {
+    setTimeout(() => {
+        fetch("https://icanhazdadjoke.com/", {
+            headers: {
+                Accept: "application/json", // Request JSON format
+            },
+        })
+            .then((response) => response.json()) // Parse the response as JSON
+            .then((data) => {
+                console.log(data); // Log the joke data
+                // Now you can work with the JSON data
+                const joke = document.querySelector(".jokes")
+                joke.textContent = data.joke
+            })
+            .catch((error) => {
+                console.error("Error fetching data:", error);
+            });
+    }, 0);
+
+
+}
+
+
+btn.addEventListener("click", jokeGenerator)
