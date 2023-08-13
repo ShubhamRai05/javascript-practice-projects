bb30757b700a1d9e610220d97641af5d50944022
Shubham Rai <shubham56237@gmail.com>
2023-08-13 14:17:10 +0530

    image api added

--- random joke generator/app.js
+++ random joke generator/app.js
@@ -1,6 +1,28 @@
 const btn = document.querySelector(".btn")
+const imgBtn = document.querySelector(".img-btn")
+const imgSrc = document.querySelector(".src-meme")
 
 
+let imageMemeCreator = async () => {
+    const url = 'https://programming-memes-images.p.rapidapi.com/v1/memes';
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'ca208249b8msh17e937264f0bde8p1b2974jsna36d3d0028a9',
+            'X-RapidAPI-Host': 'programming-memes-images.p.rapidapi.com'
+        }
+    };
+
+    try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        imgSrc.src = result[Math.floor(Math.random() * 12)].image
+
+    } catch (error) {
+        console.error(error);
+    }
+}
+imageMemeCreator()
 let jokeGenerator = () => {
     setTimeout(() => {
         fetch("https://icanhazdadjoke.com/", {
@@ -10,7 +32,7 @@ let jokeGenerator = () => {
         })
             .then((response) => response.json()) // Parse the response as JSON
             .then((data) => {
-                console.log(data); // Log the joke data
+                // console.log(data); // Log the joke data
                 // Now you can work with the JSON data
                 const joke = document.querySelector(".jokes")
                 joke.textContent = data.joke
@@ -25,3 +47,4 @@ let jokeGenerator = () => {
 
 
 btn.addEventListener("click", jokeGenerator)
+imgBtn.addEventListener("click", imageMemeCreator)
